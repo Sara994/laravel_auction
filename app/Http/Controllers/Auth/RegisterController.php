8ce66f55c074f531f53166ca91a2f7c67e71c97a
alsoghayer-astc <alsoghayer@astc.com.sa>
2018-04-29 23:05:06 +0300

    s

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,7 @@ class RegisterController extends Controller
             'username' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
-            'phone' => 'required|string|min:10|max:10|confirmed',
+            'phone' => 'required|string|min:10|max:10',
         ]);
     }
 
